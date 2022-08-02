3c225c611913fa9c04bb76d7cb1e3f1d1a7d9080
Viktoria Palma <109968018+ViktoriaPalma@users.noreply.github.com>
2022-08-02 00:07:34 +0000

    FINISHED "LEARNING THE MAORI QUIZ" Added answers to go with the questions when user picks the learn option.

--- main.py
+++ main.py
@@ -14,17 +14,35 @@ for i in range(len(quizLines)):
   quizLines[i] = quizLines[i].split(",")
 for aline in quizLines:
   listMaori.append(aline[0])
+  #print(aline)
 
-#testing
+#Getting the answers from the txtfile
+randomtest = []
+#Get rid of "\n" in each line due to the nature of the txt file
 for aline in quizLines:
-  randomtest = aline[5]
-  #print(randomtest)
-  testvalue = (ord(randomtest)-96)
-  print(testvalue)
-#print(testvalue)
+  randomtest = aline[5].strip("\n")
+  #Seperate Answer title from the rest of the list because I only want the letters 
+  if randomtest == "Answer":
+    listAnswers.append(randomtest)
+  else:
+    #Get the alphabetic number
+    #Subtract by 64 to make the Ascii into alphabet number
+    x = ord(randomtest)-64
+    #Ensure my answers are unique before adding it to my listAnswers 
+    for quack in aline:
+      quack = aline[x]
+      if quack not in listAnswers:
+        listAnswers.append(quack)
+        
 
+
+
+
+ 
 #Main menu
+
 def main():
+  #Title
   print("Maori Multiple choice quiz")
   #Menu 
   userinp = 0
@@ -44,12 +62,15 @@ def learnMaori():
   global listMaori
   #Print the Maori and english words to be learned
   for i in range(len(listMaori)):
-    print(listMaori[i])
-  #print("What would you like to do now?")
+    print(listMaori[i].ljust(20),listAnswers[i])
+
     
 
 
+
   
+def quizMaori():
+  pass
 if __name__ == '__main__':
   main()
  
